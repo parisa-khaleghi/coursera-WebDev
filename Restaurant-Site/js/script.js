d6ff81f22e4c579fb8f486f879ec8bdd549524bb
Parisa Khaleghi <parisa.khaleghi89@gmail.com>
2021-12-13 22:13:50 +0300

    Feat: Dynamic home page content
    Use js and Ajax to load HomePage content dynamic. Move the content from main-content element to a new file named home-snippet.html in snippets directory. Then use js and ajax to load content from home-snippet.html to main-content element in home page (index.html). Use a gif photo as show loading.

--- Restaurant-Site/js/script.js
+++ Restaurant-Site/js/script.js
@@ -8,3 +8,33 @@ $(function () {
 		}
 	});
 });
+
+(function (global){
+	var dc = {};
+
+	var homehtml = "../snippets/home-snippet.html";
+
+	// Convinience function for inserting innerHTML for 'select'
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	// Show loading icon inside element identified by 'selector'
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html)
+	};
+
+	// On page load (before images or CSS)
+	document.addEventListener("DOMContentLoaded", function (event) {
+		// On first load show home view
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(homehtml, function (responseText) {
+			document.querySelector("#main-content").innerHTML = responseText;
+		}, false );
+	});
+
+	global.$dc = dc;
+}) (window);
