2c29e27fb84fc19bed43ad827efc670134894d8d
Parisa Khaleghi <parisa.khaleghi89@gmail.com>
2021-12-14 12:41:58 +0300

    Fix: Fix URL for github
    Consider that URL in github is different by localhost. Add './' in other urls for snippet files in script.js

--- Restaurant-Site/js/script.js
+++ Restaurant-Site/js/script.js
@@ -14,11 +14,11 @@ $(function () {
 
 	var homehtml = "./snippets/home-snippet.html";
 	var allCategoriesURL = "http://davids-restaurant.herokuapp.com/categories.json";
-	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-	var categoryHtml = "snippets/category-snippet.html";
+	var categoriesTitleHtml = "./snippets/categories-title-snippet.html";
+	var categoryHtml = "./snippets/category-snippet.html";
 	var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
-	var menuItemsTitleHtml = "snippets/menu-items-title.html";
-	var menuItemHtml = "snippets/menu-item.html";
+	var menuItemsTitleHtml = "./snippets/menu-items-title.html";
+	var menuItemHtml = "./snippets/menu-item.html";
 
 	// Convinience function for inserting innerHTML for 'select'
 	var insertHtml = function (selector, html) {
