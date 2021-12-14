622a189a958dc7366c20dd90177a53ab4da44eab
Parisa Khaleghi <parisa.khaleghi89@gmail.com>
2021-12-14 12:45:34 +0300

    Fix: Fix URL for github
    Consider that URL in github is different by localhost. Change 'http' to 'https' for api urls in script.js

--- Restaurant-Site/js/script.js
+++ Restaurant-Site/js/script.js
@@ -13,10 +13,10 @@ $(function () {
 	var dc = {};
 
 	var homehtml = "./snippets/home-snippet.html";
-	var allCategoriesURL = "http://davids-restaurant.herokuapp.com/categories.json";
+	var allCategoriesURL = "https://davids-restaurant.herokuapp.com/categories.json";
 	var categoriesTitleHtml = "./snippets/categories-title-snippet.html";
 	var categoryHtml = "./snippets/category-snippet.html";
-	var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+	var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 	var menuItemsTitleHtml = "./snippets/menu-items-title.html";
 	var menuItemHtml = "./snippets/menu-item.html";
 
