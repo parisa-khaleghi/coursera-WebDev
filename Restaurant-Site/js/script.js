f262b2af49637fe4415af198cdeb4ff3517c4603
Parisa Khaleghi <parisa.khaleghi89@gmail.com>
2021-12-15 13:17:56 +0300

    Fix: Fix return value
    Fix return value in 'randomCategoryShortName' function, it should be a value not an index.

--- Restaurant-Site/js/script.js
+++ Restaurant-Site/js/script.js
@@ -69,7 +69,7 @@ $(function () {
 		var menu = ["A", "B" "C", "C2", "Cm", "CSR", "CU", "D", 
 					"Dk", "DS", "F", "Fr", "FY", "L", "NF", "NL", 
 					"NS", "Pf", "SO", "Sp", "SR", "SS", "T", "V", "Vg"];
-		return Math.floor(Math.random() * menu.length);
+		return menu[Math.floor(Math.random() * menu.length)];
 	}
 
 	// Load thhe menu categories view
